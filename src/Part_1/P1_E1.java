046282ea6f13d4a4745188ce2d06ae541e8ffcc4
Dmitriy <dimaliahov@gmail.com>
2019-08-02 12:51:07 +0300

    Message

---
+++ src/Part_1/P1_E1.java
@@ -0,0 +1,7 @@
+package Part_1;
+
+public class P1_E1 {
+    public static void main(String[] args) {
+        System.out.println("Hi!! There...");
+    }
+}
--- src/S1_E1.java
+++
@@ -1,5 +0,0 @@
-public class S1_E1 {
-    public static void main(String[] args) {
-
-    }
-}
